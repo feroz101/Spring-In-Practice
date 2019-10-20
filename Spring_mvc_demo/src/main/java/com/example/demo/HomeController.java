27899e1b712094d2f15b2abfb298ff6b3e5b27b3
feroz101 <ferozmujawar101@gmail.com>
2019-10-20 20:05:31 +0530

    initial

---
+++ Spring_mvc_demo/src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,35 @@
+package com.example.demo;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Handles requests for the application home page.
+ */
+
+@RestController
+@RequestMapping("/api") 
+public class HomeController {
+
+	@Autowired 
+	@Qualifier("s1")
+	Student student;
+	
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@GetMapping(value = "/home")
+	public @ResponseBody Student home() {
+  	 return student;
+   } 
+	
+}
---
+++ Spring_mvc_demo/src/main/java/com/example/demo/Student.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+public class Student {
+
+	int id;
+	String name;
+	
+	Student(){
+		
+	}
+	
+	
+	public Student(int id,String name) {
+		this.id=id;this.name=name;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+}
+ 
